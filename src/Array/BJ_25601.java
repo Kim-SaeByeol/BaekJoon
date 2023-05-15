1962b186813c58bf02b4dea61cc9001cc4c55367
Kim-SaeByeol <toquf345@gmail.com>
2023-05-15 23:54:27 +0900

    23-05-15 형변환 클래스 상속 형식풀이

---
+++ src/Array/BJ_25601.java
@@ -0,0 +1,48 @@
+package Array;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class BJ_25601 {
+    HashMap<String, ArrayList<String>> edges;
+
+    private boolean bf(String cur, String ed) {
+        if (cur.equals(ed)) {
+            return true;
+        }
+        if (edges.get(cur) == null)
+            return false;
+        for (String next : edges.get(cur)) {
+            if (bf(next, ed))
+                return true;
+        }
+        return false;
+    }
+
+    private void solution() throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        edges = new HashMap<>();
+        for (int i = 0; i < n-1; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String a = st.nextToken();
+            String b = st.nextToken();
+            if (!edges.containsKey(a)) {
+                edges.put(a, new ArrayList<>());
+            }
+            edges.get(a).add(b);
+        }
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        String a = st.nextToken();
+        String b = st.nextToken();
+        System.out.println(bf(a, b) || bf(b, a) ? 1 : 0);
+    }
+
+    public static void main(String[] args) throws Exception {
+        new BJ_25601().solution();
+    }
+}
