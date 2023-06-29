b859c514ddfa8be4942ce6556023e21778b145fd
Kim-SaeByeol <toquf345@gmail.com>
2023-06-29 23:56:23 +0900

    23-06-29 백준풀이

---
+++ src/input_output/BJ_11382.java
@@ -0,0 +1,14 @@
+package input_output;
+
+import java.util.Scanner;
+
+public class BJ_11382 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long A = sc.nextLong();
+        long B = sc.nextLong();
+        long C = sc.nextLong();
+
+        System.out.println(A + B + C);
+    }
+}
