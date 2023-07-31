5baaf252cdc664e8d8406fb2cb6d4c5292238975
Kim-SaeByeol <toquf345@gmail.com>
2023-07-31 23:41:22 +0900

    일본여행 이후) 23.07.31 백준풀이

---
+++ src/string/BJ_11718.java
@@ -0,0 +1,15 @@
+package string;
+
+import java.util.Scanner;
+
+public class BJ_11718 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        while (sc.hasNextLine()) {
+            String st = sc.nextLine();
+            System.out.println(st);
+        }
+        sc.close();
+    }
+}
---
+++ src/string/BJ_2908.java
@@ -0,0 +1,21 @@
+package string;
+
+import java.util.Scanner;
+
+public class BJ_2908 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+
+
+        A = Integer.parseInt(new StringBuilder().append(A).reverse().toString());
+        B = Integer.parseInt(new StringBuilder().append(B).reverse().toString());
+
+        System.out.print(A > B ? A : B);
+        sc.close();
+
+    }
+}
